8e4df7d73f68a76239f654f5c77422d6d8d02723
Nathan Braun <nathanericbraun@gmail.com>
2023-09-08 09:45:27 -0500

    Improve league setup file

--- hosts/league_setup.py
+++ hosts/league_setup.py
@@ -1,4 +1,3 @@
-import hosts.fleaflicker as site
 from hosts.db import overwrite_league
 from pandas import DataFrame
 import sqlite3
@@ -8,31 +7,56 @@ from utilities import DB_PATH
 # update all this for your league
 # need to do for EVERY league you analyze
 #########################################
-LEAGUE_ID = 0  
-TEAM_ID = 0  
+LEAGUE_ID = 34958
+TEAM_ID = 217960
 
-LEAGUE_NAME = "Family League"
+LEAGUE_NAME = "BB Bowl"
 HOST = 'fleaflicker'
-SCORING = {'qb': 'pass4', 'skill': 'ppr0', 'dst': 'mfl'}
+SCORING = {'qb': 'pass_6', 'skill': 'ppr_1', 'dst': 'dst_high'}
 
 ##################################################
 # shouldn't have to change anything from here down
 ##################################################
 
-# open up our database connection
-conn = sqlite3.connect(DB_PATH)
+# do some checks
+assert SCORING['qb'] in ['pass_4', 'pass_6']
+assert SCORING['skill'] in ['ppr_1', 'ppr_1over2', 'ppr_0']
+assert SCORING['dst'] in ['dst_std', 'dst_high']
 
-# team list
-teams = site.get_teams_in_league(LEAGUE_ID)
-overwrite_league(teams, 'teams', conn, LEAGUE_ID)
+# load right helper functions depending on platform
+match HOST.lower():
+    case 'fleaflicker':
+        import hosts.fleaflicker as site
+    case 'espn':
+        import hosts.espn as site
+    case 'yahoo':
+        import hosts.yahoo as site
+    case 'sleeper':
+        import hosts.sleeper as site
+    case _:
+        raise ValueError(f"Unknown host: {HOST}")
 
-# schedule info
-schedule = site.get_league_schedule(LEAGUE_ID)
-overwrite_league(schedule, 'schedule', conn, LEAGUE_ID)
+if __name__ == '__main__':
+    # work starts here:
+    # open up our database connection
+    conn = sqlite3.connect(DB_PATH)
 
-# league info
-league = DataFrame([{'league_id': LEAGUE_ID, 'team_id': TEAM_ID, 'host':
-                     HOST.lower(), 'name': LEAGUE_NAME, 'qb_scoring':
-                     SCORING['qb'], 'skill_scoring': SCORING['skill'],
-                     'dst_scoring': SCORING['dst']}])
-overwrite_league(league, 'league', conn, LEAGUE_ID)
+    # team list
+    teams = site.get_teams_in_league(LEAGUE_ID)
+    overwrite_league(teams, 'teams', conn, LEAGUE_ID)
+
+    # schedule info
+    schedule = site.get_league_schedule(LEAGUE_ID)
+    overwrite_league(schedule, 'schedule', conn, LEAGUE_ID)
+
+
+    # league info
+    league = DataFrame([{'league_id': LEAGUE_ID,
+                         'team_id': TEAM_ID,
+                         'host': HOST.lower(),
+                         'name': LEAGUE_NAME,
+                         'qb_scoring': SCORING['qb'],
+                         'skill_scoring': SCORING['skill'],
+                         'dst_scoring': SCORING['dst']}])
+
+    overwrite_league(league, 'league', conn, LEAGUE_ID)
