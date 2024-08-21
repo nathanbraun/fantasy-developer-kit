654d36e57e8c3e267a270033da07db401df0bad3
Nathan Braun <nathanericbraun@gmail.com>
2024-08-21 05:00:24 -0500

    feat: update example league_setup.py

--- hosts/league_setup.py
+++ hosts/league_setup.py
@@ -11,14 +11,9 @@ from utilities import DB_PATH
 LEAGUES = {
     'nate-league': {
         'host': 'fleaflicker',
-        'league_id': 316893,
-        'team_id': 1605156,
-        'scoring': {'qb': 'pass_4', 'skill': 'ppr_0', 'dst': 'dst_std'}},
-    'bball': {
-        'host': 'sleeper',
-        'league_id': 1002102487509295104,
-        'team_id': 4,
-        'scoring': {'qb': 'pass_6', 'skill': 'ppr_1', 'dst': 'dst_std'}},
+        'league_id': 34958,
+        'team_id': 217960,
+        'scoring': {'qb': 'pass_6', 'skill': 'ppr_1', 'dst': 'dst_high'}},
     # add new leagues here in same format (uncomment)
     # 'your-league': {
     #     'host': 'espn',
